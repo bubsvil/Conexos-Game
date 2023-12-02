2a57bdc34df9868e561d4ed5ec8a989f20059435
bubsvil <brunamartinsvilete@gmail.com>
2023-12-02 10:57:36 -0300

    Adicionando integração ranking home

---
+++ JavaScript/ranking-jogo1.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+async function fetchData() {
+  try {
+    const response = await axios.get('URL_DO_SEU_BACKEND'); // Substitua pela URL do seu backend
+    const data = response.data;
+
+    updateTable(data);
+  } catch (error) {
+    console.error('Erro ao obter dados do backend', error);
+  }
+}
+
+function updateTable(data) {
+  const tableBody = document.getElementById('ranking-jogo1'); 
+
+  tableBody.innerHTML = '';
+
+  data.forEach(item => {
+    const row = document.createElement('tr');
+
+   
+    const positionCell = document.createElement('td');
+    positionCell.textContent = item.position;
+
+    const scoreCell = document.createElement('td');
+    scoreCell.textContent = item.score;
+
+    const usernameCell = document.createElement('td');
+    usernameCell.textContent = item.username;
+
+    row.appendChild(positionCell);
+    row.appendChild(scoreCell);
+    row.appendChild(usernameCell);
+
+    tableBody.appendChild(row);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', fetchData);
